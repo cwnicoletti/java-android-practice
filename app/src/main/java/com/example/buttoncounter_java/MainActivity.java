9d7e1c3b3c4c5b86b166f17a5102e95c65987b80
christiannicoletti <cwnicoletti@gmail.com>
2021-07-20 14:10:37 -0700

    User can now input text, click the button, and it's displayed in the TextView

--- app/src/main/java/com/example/buttoncounter_java/MainActivity.java
+++ app/src/main/java/com/example/buttoncounter_java/MainActivity.java
@@ -3,13 +3,38 @@ package com.example.buttoncounter_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userInput;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editTextTextPersonName);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        userInput.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.append(userInput.getText() + "\n");
+                userInput.setText("");
+            }
+        };
+        if (button != null) {
+            button.setOnClickListener(ourOnClickListener);
+        }
     }
 }
